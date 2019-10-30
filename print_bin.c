bc1f2b440b2f600675af6f151606ae4b3818b5e8
Elnur Sultanov <esultanov6@gmail.com>
2019-10-30 09:53:43 +0000

    convert dec to bin

---
+++ print_bin.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_bin - converts dec to bin.
+ * @args: argument.
+ * Return: binary value.
+ */
+int print_bin(va_list args)
+{
+	int d = va_arg(args, int);
+	int bin[32];
+	int i=0;
+
+	while (d > 0)
+	{
+		bin[i] = d % 2;
+		d = d / 2;
+		i++;
+	}
+	for (int j = i - 1; j >= 0; j--)
+		putchar(bin[j]+ 48);
+	return (0);
+}
