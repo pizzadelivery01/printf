97dddd678c17345ea3591cbeb67cb3d9ebb3d429
Elnur Sultanov <esultanov6@gmail.com>
2019-11-04 23:42:35 +0000

    man edited

--- print_bin.c
+++ print_bin.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "holberton.h"
 /**
  * print_bin - converts dec to bin.
@@ -9,14 +11,16 @@ int print_bin(va_list args)
 	int d = va_arg(args, int);
 	int bin[32];
 	int i=0;
+	int count = 0;
 
 	while (d > 0)
 	{
 		bin[i] = d % 2;
 		d = d / 2;
 		i++;
+		count++;
 	}
 	for (int j = i - 1; j >= 0; j--)
 		putchar(bin[j]+ 48);
-	return (0);
+	return (count);
 }
